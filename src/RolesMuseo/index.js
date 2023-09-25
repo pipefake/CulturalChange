adad556c475e82c999eb147950d6d133213289f9
Juan Sebastian Galeano A <juan_seb.galeano@uao.edu.co>
2023-09-25 17:54:00 -0500

    RestructuracionRolesMuseo

--- src/RolesMuseo/index.js
+++ src/RolesMuseo/index.js
@@ -1,38 +1,32 @@
 import React from "react";
 import "./RolesMuseo.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
 import GuiaLogo from "./resources/GuiaLogo.png";
 import HuaqueroLogo from "./resources/HuaqueroLogo.png";
 import InterpreteLogo from "./resources/InterpreteLogo.png";
 import AntropologoLogo from "./resources/AntropologoLogo.png";
+import FondoRoles from "./resources/FondoRoles.png";
 
 const roles = [
   {
     titulo: "Guía",
-    color: "#A6DD03",
     descripcion:
       "Deberás observar detalladamente el mapa e indicarle al Huaquero, dónde debe buscar los misteriosos símbolos que aparecen en el mapa.",
     logo: GuiaLogo,
   },
   {
     titulo: "Huaquero",
-    color: "#9A0000",
     descripcion:
       "Con ayuda de la cámara de tu dispositivo, escanea las esculturas y descubre los símbolos misteriosos.",
     logo: HuaqueroLogo,
   },
   {
     titulo: "Intérprete",
-    color: "#2C53A2",
     descripcion:
       "Utiliza los símbolos que el Huaquero encuentre para traducirlos. Encuentra sus pares para revelar su significado.",
     logo: InterpreteLogo,
   },
   {
     titulo: "Antropólogo",
-    color: "#D9771E",
     descripcion:
       "Resuelve el anagrama, ordena las palabras para desbloquear el código secreto.",
     logo: AntropologoLogo,
@@ -41,22 +35,25 @@ const roles = [
 
 function RolesMuseo() {
   return (
-    <Slider className="roles-slider">
-      {roles.map((rol, index) => (
-        <div
-          key={index}
-          className="role-slide"
-          style={{ backgroundImage: `url(${rol.fondo})` }}
-        >
-          <h1>¡Seleccionen sus roles, viajeros!</h1>
-          <h2>{rol.titulo}</h2>
-          <div className="role-content">
-            <img src={rol.logo} alt={rol.titulo} />
-            <p>{rol.descripcion}</p>
+    <div className="roles-section">
+      <div
+        className="fondo"
+        style={{ backgroundImage: `url(${FondoRoles})` }}
+      ></div>
+      <h1>¡Seleccionen sus roles, viajeros!</h1>
+      <h3>Estamos a punto de comenzar.</h3>
+      <div className="roles-container">
+        {roles.map((rol, index) => (
+          <div key={index} className="role">
+            <h2>{rol.titulo}</h2>
+            <div className="role-content">
+              <img src={rol.logo} alt={rol.titulo} />
+              <p>{rol.descripcion}</p>
+            </div>
           </div>
-        </div>
-      ))}
-    </Slider>
+        ))}
+      </div>
+    </div>
   );
 }
 

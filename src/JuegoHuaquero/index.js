56da7810d87fcafd724a1f3e5c38bbdb02d3aff0
Juan Sebastian Galeano A <juan_seb.galeano@uao.edu.co>
2023-10-06 04:39:05 -0500

    Actualizacion2

--- src/JuegoHuaquero/index.js
+++ src/JuegoHuaquero/index.js
@@ -1,55 +1,38 @@
 import React, { useState, useEffect } from "react";
+import "aframe";
+import "aframe-ar";
 import "./JuegoHuaquero.css";
+import markerURL from "./resources/marker.png"; // Importa el marcador
+import imageUrl from "./resources/pruebaRA.png"; // Importa la imagen
 
 function JuegoHuaquero() {
   const [arEnabled, setArEnabled] = useState(false);
 
   useEffect(() => {
     if (arEnabled) {
-      const scene = new window.THREE.Scene();
-      const camera = new window.THREE.Camera();
+      // Configura la escena de A-Frame
+      const scene = document.createElement("a-scene");
+      scene.setAttribute("embedded", "");
 
-      const renderer = new window.THREE.WebGLRenderer({
-        antialias: true,
-        alpha: true,
-      });
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      document.getElementById("ar-container").appendChild(renderer.domElement);
+      // Crea un marcador
+      const marker = document.createElement("a-marker");
+      marker.setAttribute("type", "pattern");
+      marker.setAttribute("url", markerURL); // Utiliza el marcador importado
 
-      // Configurar el marcador como una imagen
-      const markerControls = new window.ARMarkerControls(
-        window.ARController.getUserMediaThreeScene({
-          cameraParametersUrl: "data/camera_para.dat",
-          maxARVideoSize: 640,
-        }),
-        "./resources/marker.png", // Ruta a tu imagen de marcador
-        {
-          type: "pattern",
-          patternUrl: "./resources/marker.png", // Ruta a tu archivo .patt (para patrones)
-        }
-      );
+      // Crea una imagen en realidad aumentada
+      const arImage = document.createElement("a-image");
+      arImage.setAttribute("src", imageUrl); // Utiliza la imagen importada
+      arImage.setAttribute("width", "2"); // Configura el ancho de la imagen
+      arImage.setAttribute("height", "2"); // Configura la altura de la imagen
 
-      // Cargar objetos virtuales (imágenes en este caso)
-      const loader = new window.THREE.TextureLoader();
-      loader.load("./resources/pruebaRA.png", (texture) => {
-        const geometry = new window.THREE.PlaneGeometry(1, 1);
-        const material = new window.THREE.MeshBasicMaterial({ map: texture });
-        const mesh = new window.THREE.Mesh(geometry, material);
-        scene.add(mesh);
-      });
+      // Agrega la imagen al marcador
+      marker.appendChild(arImage);
 
-      // Configurar la cámara
-      const arCamera = markerControls.getCamera();
-      arCamera.position.z = 1;
+      // Agrega el marcador a la escena
+      scene.appendChild(marker);
 
-      // Iniciar renderizado
-      const animate = () => {
-        if (markerControls.isVisible) {
-          renderer.render(scene, arCamera);
-        }
-        requestAnimationFrame(animate);
-      };
-      animate();
+      // Agrega la escena al DOM
+      document.getElementById("ar-container").appendChild(scene);
     }
   }, [arEnabled]);
 

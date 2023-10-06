d9ed9d431351580096929cff8c86535c37c548e9
pipefake <jfjimenezsalazar@gmail.com>
2023-10-05 23:28:57 -0500

    implementación de fechData

--- src/App.js
+++ src/App.js
@@ -40,7 +40,6 @@ function App() {
             <Route path="/introduccion/:slug" element={<Introduccion />} />
             <Route path="/rolesMuseo" element={<RolesMuseo />} />
             <Route path="/seleccionCargando" element={<SeleccionCargando />} />
-
             <Route path="/juego/guia" element={<Mapa historia={4} />} />
             <Route path="/juego/interprete/parejas" element={<Minijuego historia={1}></Minijuego>} />
             <Route path="/juego/interprete/traduccion" element={<Traductor historia={1}></Traductor>} />
--- src/Picker/index.js
+++ src/Picker/index.js
@@ -18,8 +18,49 @@ import logoInterpreteBN from '../SeleccionCargando/logos/logoInterpreteBN.png';
 import logoAntropologoBN from '../SeleccionCargando/logos/logoAntropologoBN.png';
 
 function Picker() {
+    const [userData, setUserData] = useState({
+        _id: "",
+        name: "",
+        identification: "",
+        email: "",
+        rol: "",
+        finalizadaTarea: "false",
+        tipoUsuario: "",
+    });
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await getUserData(); // asssuming this function fetches user data
+                if (data) {
+                    setUserData(data);
+                    console.log('User data set:', data);
+                } else {
+                    console.error('No user data received');
+                }
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
 
+        fetchData(); // invoke the function to fetch and set user data
+    }, []);
+
+    const userId = localStorage.getItem("userId"); // ID from local storage
+    const getUserData = async () => {
+        try {
+            const response = await axios.get("/users"); // Adjusted the endpoint
+            const users = response.data;
+            const user = users.find((u) => u._id === userId); // Assuming each user object has an _id field
+            if (user) {
+                return user;
+            } else {
+                console.error("User not found");
+            }
+        } catch (error) {
+            console.error("Error fetching users:", error);
+        }
+    };
     return (
         <>
             <div className="divPicker">

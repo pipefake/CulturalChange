bc26d9af306a5196116d01795ddbe6799ee73369
Juan Sebastian Galeano A <juan_seb.galeano@uao.edu.co>
2023-10-24 18:14:58 -0500

    Correcion

--- src/Traductor/Minijuego.js
+++ src/Traductor/Minijuego.js
@@ -42,7 +42,7 @@ import zacru from "./anagramas/zacru.png";
 import dosniso from "./anagramas/dosniso.png";
 import zaspie from "./anagramas/zaspie.png";
 import batosil from "./anagramas/batosil.png";
-import parejaCorrecta from './audios/parejaCorrecta.mp3';
+import parejaCorrecta from "./audios/parejaCorrecta.mp3";
 import { useMyContext } from "../SeleccionCargando/MyContext";
 import { Traductor } from "./index.js";
 
@@ -51,7 +51,6 @@ import { simbolos } from "../rolesdata.js";
 
 import { Contexto } from "../Contexto";
 import { Acumulador } from "./Acumulador";
-
 
 // Agrega más imágenes según la cantidad de elementos en tu array original
 
@@ -78,7 +77,6 @@ const Minijuego = (props) => {
   const [showThirdModal, setShowThirdModal] = useState(false);
   const [thirdModalClosed, setThirdModalClosed] = useState(false);
 
-
   useEffect(() => {
     if (modalClosed) {
       setShowModal(false);
@@ -88,7 +86,6 @@ const Minijuego = (props) => {
   function closeModal() {
     setModalClosed(true);
   }
-
 
   function closeSecondModal() {
     setSecondModalClosed(true);
@@ -113,7 +110,7 @@ const Minijuego = (props) => {
   useEffect(() => {
     const intervalId = setInterval(() => {
       fetchSymbols();
-      console.log(encontrados)
+      console.log(encontrados);
     }, 10000); // Check every 10 seconds
 
     return () => clearInterval(intervalId); // Clear the interval on unmount
@@ -268,8 +265,6 @@ const Minijuego = (props) => {
     setShuffledMemoBlocks(shuffledMemoBlocksCopy);
     if (selectedMemoBlock === null) {
       setselectedMemoBlock(memoBlock);
-
-
     } else if (selectedMemoBlock.image === memoBlock.image) {
       console.log(
         `¡Las parejas coinciden! Son: ${JSON.stringify(
@@ -278,7 +273,6 @@ const Minijuego = (props) => {
       );
       const positionInImageList = imageList.indexOf(selectedMemoBlock.image);
       console.log(positionInImageList);
-
 
       if (positionInImageList < 4) {
         if (encontrados[positionInImageList]) {
@@ -290,12 +284,10 @@ const Minijuego = (props) => {
       }
       if (positionInImageList === 0 && encontrados[positionInImageList]) {
         setselectedMemoBlock(null);
-
       } else if (
         !imageList.slice(0, 4).includes(selectedMemoBlock.image) ||
         !encontrados[positionInImageList]
       ) {
-
         if (!encontrados[positionInImageList]) {
           setAnimating(true);
           setTimeout(() => {
@@ -308,136 +300,133 @@ const Minijuego = (props) => {
             setShuffledMemoBlocks(shuffledMemoBlocksCopy);
             setselectedMemoBlock(null);
             setAnimating(false);
-
-
           }, 1000);
         } else {
-
         }
         if (!imageList.slice(0, 4).includes(selectedMemoBlock.image)) {
           setShowSecondModal(true);
+        } else {
+          setselectedMemoBlock(null);
+        }
       } else {
-        setselectedMemoBlock(null);
-
+        setAnimating(true);
+        setTimeout(() => {
+          shuffledMemoBlocksCopy.splice(memoBlock.index, 1, memoBlock);
+          shuffledMemoBlocksCopy.splice(
+            selectedMemoBlock.index,
+            1,
+            selectedMemoBlock
+          );
+          setShuffledMemoBlocks(shuffledMemoBlocksCopy);
+          setselectedMemoBlock(null);
+          setAnimating(false);
+        }, 1000);
       }
-
-
-
-    } else {
-
-      setAnimating(true);
-      setTimeout(() => {
-        shuffledMemoBlocksCopy.splice(memoBlock.index, 1, memoBlock);
-        shuffledMemoBlocksCopy.splice(
-          selectedMemoBlock.index,
-          1,
-          selectedMemoBlock
-        );
-        setShuffledMemoBlocks(shuffledMemoBlocksCopy);
-        setselectedMemoBlock(null);
-        setAnimating(false);
-
-      }, 1000);
     }
-  };
 
-  const validardor = (index) => {
-    let aux;
+    const validardor = (index) => {
+      let aux;
 
-    if (index === 0) {
-      aux = esinterpretado1;
-    } else if (index === 1) {
-      aux = esinterpretado2;
-    } else if (index === 2) {
-      aux = esinterpretado3;
-    } else if (index === 3) {
-      aux = esinterpretado4;
-    }
-    return aux;
-  };
+      if (index === 0) {
+        aux = esinterpretado1;
+      } else if (index === 1) {
+        aux = esinterpretado2;
+      } else if (index === 2) {
+        aux = esinterpretado3;
+      } else if (index === 3) {
+        aux = esinterpretado4;
+      }
+      return aux;
+    };
 
-  return (
-    <>
-      {showModal && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={closeModal}>
-              &times;
-            </span>
-            <p>
-              Espera a que el huaquero encuentre los símbolos misteriosos. Ten
-              cuidado si descubres símbolos diferentes, perderás tiempo.
-            </p>
+    return (
+      <>
+        {showModal && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={closeModal}>
+                &times;
+              </span>
+              <p>
+                Espera a que el huaquero encuentre los símbolos misteriosos. Ten
+                cuidado si descubres símbolos diferentes, perderás tiempo.
+              </p>
+            </div>
           </div>
-        </div>
-      )}
-      {showSecondModal && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={() => setShowSecondModal(false)}>
-              &times;
-            </span>
-            <p>¡Ups! Este símbolo no pertenece a la historia. Perderás x minutos.</p>
+        )}
+        {showSecondModal && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={() => setShowSecondModal(false)}>
+                &times;
+              </span>
+              <p>
+                ¡Ups! Este símbolo no pertenece a la historia. Perderás x
+                minutos.
+              </p>
+            </div>
           </div>
-        </div>
-      )}
-      {showThirdModal && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={() => setShowThirdModal(false)}>
-              &times;
-            </span>
-            <p>Este símbolo aun no ha sido encontrado por el huaquero, tendrás que esperar</p>
+        )}
+        {showThirdModal && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={() => setShowThirdModal(false)}>
+                &times;
+              </span>
+              <p>
+                Este símbolo aun no ha sido encontrado por el huaquero, tendrás
+                que esperar
+              </p>
+            </div>
           </div>
-        </div>
-      )}
-      <Contexto
-        titulo="Descubre los símbolos"
-        parrafo="Ten cuidado, si descubres símbolos diferentes a los que el Huaquero te mostró, perderás los que has descubierto"
-      ></Contexto>
-      <div className="fondoAmarillo">
-        <div className="contentMinijuego">
-          <button
-            className="btnInterpreteSlide"
-            onClick={cambiarComponenteInterprete}
-          >
-            {!btnSlide ? (
-              <img src={off} alt="logo de Guia" />
-            ) : (
-              <img src={on} alt="logo de Guia" />
-            )}
-          </button>
-          <div className="ContTraduccion">
-            {btnSlide ? (
-              anagramas.map((simbolo, index) => (
-                <Traductor
-                  key={index}
-                  historia={props.historia}
-                  imgAnagrama={anagramas[index]}
-                  imgSimbolo={imageList[index]}
-                  valid={validardor(index)}
+        )}
+        <Contexto
+          titulo="Descubre los símbolos"
+          parrafo="Ten cuidado, si descubres símbolos diferentes a los que el Huaquero te mostró, perderás los que has descubierto"
+        ></Contexto>
+        <div className="fondoAmarillo">
+          <div className="contentMinijuego">
+            <button
+              className="btnInterpreteSlide"
+              onClick={cambiarComponenteInterprete}
+            >
+              {!btnSlide ? (
+                <img src={off} alt="logo de Guia" />
+              ) : (
+                <img src={on} alt="logo de Guia" />
+              )}
+            </button>
+            <div className="ContTraduccion">
+              {btnSlide ? (
+                anagramas.map((simbolo, index) => (
+                  <Traductor
+                    key={index}
+                    historia={props.historia}
+                    imgAnagrama={anagramas[index]}
+                    imgSimbolo={imageList[index]}
+                    valid={validardor(index)}
+                  />
+                ))
+              ) : (
+                <Board
+                  memoBlocks={shuffledMemoBlocks}
+                  animating={animating}
+                  handleMemoClick={handleMemoClick}
                 />
-              ))
-            ) : (
-              <Board
-                memoBlocks={shuffledMemoBlocks}
-                animating={animating}
-                handleMemoClick={handleMemoClick}
-              />
-            )}
+              )}
+            </div>
           </div>
         </div>
-      </div>
 
-      <Acumulador
-        historia={props.historia}
-        encontrado1={encontrados[0]}
-        encontrado2={encontrados[1]}
-        encontrado3={encontrados[2]}
-        encontrado4={encontrados[3]}
-      />
-    </>
-  );
+        <Acumulador
+          historia={props.historia}
+          encontrado1={encontrados[0]}
+          encontrado2={encontrados[1]}
+          encontrado3={encontrados[2]}
+          encontrado4={encontrados[3]}
+        />
+      </>
+    );
+  };
 };
-
 export { Minijuego };

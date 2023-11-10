7a7d1bc38f6b64bf5a4407a22918f9018c5ed3d7
pipefake <jfjimenezsalazar@gmail.com>
2023-11-09 19:27:38 -0500

    huaquero

--- src/Huaquero/index.js
+++ src/Huaquero/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Huaquero.css";
 import simbolo1 from "../Traductor/simbolos/simbolo1.png";
 import simbolo2 from "../Traductor/simbolos/simbolo2.png";
@@ -17,59 +17,120 @@ import simbolo14 from "../Traductor/simbolos/simbolo14.png";
 import simbolo15 from "../Traductor/simbolos/simbolo15.png";
 import simbolo16 from "../Traductor/simbolos/simbolo16.png";
 
-function Huaquero() {
+function Huaquero(props) {
+    const [buttonStates, setButtonStates] = useState(Array(16).fill(false));
+    const [simbolos, setSimbolos] = useState([]);
+
+    const handleButtonClick = (index) => {
+        const newButtonStates = [...buttonStates];
+        newButtonStates[index] = !newButtonStates[index];
+        setButtonStates(newButtonStates);
+    };
+
+    useEffect(() => {
+        buscarUbicaciones(props.historia);
+    }, [props.historia]);
+
+    function buscarUbicaciones(aux) {
+        let newImageList = [];
+
+        if (aux === 1) {
+            newImageList = [
+                simbolo1,
+                simbolo2,
+                simbolo3,
+                simbolo4,
+            ];
+        } else if (aux === 2) {
+            newImageList = [
+                simbolo13,
+                simbolo14,
+                simbolo15,
+                simbolo16,
+
+            ];
+        } else if (aux === 3) {
+            newImageList = [
+                simbolo9,
+                simbolo10,
+                simbolo11,
+                simbolo12,
+
+            ];
+        } else if (aux === 4) {
+            newImageList = [
+                simbolo5,
+                simbolo6,
+                simbolo7,
+                simbolo8,
+
+            ];
+        }
+
+        setSimbolos([...newImageList]);
+    }
+
     return (
-        <div className="image-container">
-            <button className="image-button">
-                <img src={simbolo1} alt="Simbolo 1" />
-            </button>
-            <button className="image-button">
-                <img src={simbolo2} alt="Simbolo 2" />
-            </button>
-            <button className="image-button">
-                <img src={simbolo3} alt="Simbolo 3" />
-            </button>
-            <button className="image-button">
-                <img src={simbolo4} alt="Simbolo 4" />
-            </button>
-            <button className="image-button">
-                <img src={simbolo5} alt="Simbolo 5" />
-            </button>
-            <button className="image-button">
-                <img src={simbolo6} alt="Simbolo 6" />
-            </button>
-            <button className="image-button">
-                <img src={simbolo7} alt="Simbolo 7" />
-            </button>
-            <button className="image-button">
-                <img src={simbolo8} alt="Simbolo 8" />
-            </button>
-            <button className="image-button">
-                <img src={simbolo9} alt="Simbolo 9" />
-            </button>
-            <button className="image-button">
-                <img src={simbolo10} alt="Simbolo 10" />
-            </button>
-            <button className="image-button">
-                <img src={simbolo11} alt="Simbolo 11" />
-            </button>
-            <button className="image-button">
-                <img src={simbolo12} alt="Simbolo 12" />
-            </button>
-            <button className="image-button">
-                <img src={simbolo13} alt="Simbolo 13" />
-            </button>
-            <button className="image-button">
-                <img src={simbolo14} alt="Simbolo 14" />
-            </button>
-            <button className="image-button">
-                <img src={simbolo15} alt="Simbolo 15" />
-            </button>
-            <button className="image-button">
-                <img src={simbolo16} alt="Simbolo 16" />
-            </button>
+        <div>
+
+            <p className="parrafoInferior">
+                Toca los simbolos, tendrás que tener cuidado, si los símbolo no coinciden con la historia perderas x cantidad de tiempo.
+            </p>
+            <div className="fondoAmarillo">
+
+                <div className="image-container ">
+                    {Array.from({ length: 16 }, (_, index) => (
+                        <button
+                            key={index}
+                            className={`image-button ${buttonStates[index] ? "active" : ""}`}
+                            onClick={() => handleButtonClick(index)}
+                        >
+                            <img src={getSymbolImage(index + 1)} alt={`Simbolo ${index + 1}`} />
+                        </button>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
+
+const getSymbolImage = (symbolNumber) => {
+    switch (symbolNumber) {
+        case 1:
+            return simbolo1;
+        case 2:
+            return simbolo2;
+        case 3:
+            return simbolo3;
+        case 4:
+            return simbolo4;
+        case 5:
+            return simbolo5;
+        case 6:
+            return simbolo6;
+        case 7:
+            return simbolo7;
+        case 8:
+            return simbolo8;
+        case 9:
+            return simbolo9;
+        case 10:
+            return simbolo10;
+        case 11:
+            return simbolo11;
+        case 12:
+            return simbolo12;
+        case 13:
+            return simbolo13;
+        case 14:
+            return simbolo14;
+        case 15:
+            return simbolo15;
+        case 16:
+            return simbolo16;
+        default:
+            return null;
+    }
+};
 
 export { Huaquero };
